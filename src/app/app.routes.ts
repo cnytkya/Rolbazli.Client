7188fc3414750630d9bef9cde4f65f1464b12b6d
kayac <kayac@CUNEYT>
2025-05-12 18:33:58 +0300

    Son degisiklikler

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/clients/home/home.component';
+import { LoginComponent } from './components/admin/login/login.component';
+import { RegisterComponent } from './components/admin/register/register.component';
 
 export const routes: Routes = [
     {
