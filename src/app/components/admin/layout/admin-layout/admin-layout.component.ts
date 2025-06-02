20362d744189e75c493909e101a6f0910fc9ad06
kayac <kayac@CUNEYT>
2025-06-02 11:49:28 +0300

    admin panel tasarımı

--- src/app/components/admin/layout/admin-layout/admin-layout.component.ts
+++ src/app/components/admin/layout/admin-layout/admin-layout.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../../admin/layout/navbar/navbar.component';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [RouterOutlet,SidebarComponent, NavbarComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss'
 })
 export class AdminLayoutComponent {
+  isSidebarOpen = true;
 
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
--- src/app/components/admin/layout/navbar/navbar.component.ts
+++ src/app/components/admin/layout/navbar/navbar.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../../services/auth.service';
+import { Router } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule,MatSnackBarModule,MatIconModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  showUserDropdown = false;
+  matSnackbar = inject(MatSnackBar); //kullanıcı çıkış yaptığında bir mesaj göstermek için kullanılabilir.
+  authService = inject(AuthService)
+  router = inject(Router)
+
+  constructor( ){}
+  
+  
+  toggleUserDropdown(): void {
+    this.showUserDropdown = !this.showUserDropdown;
+  }
+
+  toggleSidebar(): void {
+    // Sidebar toggle logic will be handled via service or state management
+  }
 
 }
